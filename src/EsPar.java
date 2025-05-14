48af79a7337f5682b77d8cfca2e28746d34ad119
Adelina Trejo <kf8@hotmail.com>
2025-05-13 20:15:55 -0600

    first

---
+++ src/EsPar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Principal {
+    /*
+    Ana necesita identificar si un número es par o impar para un juego que está desarrollando.
+    Para ello, quiere crear un programa que analice si el número que ha definido previamente es par o impar.
+     */
+    public static void main(String[] args) {
+
+        Scanner scaner = new Scanner(System.in);
+        int numero = scaner.nextInt();
+
+        if(numero%2 == 0){
+            System.out.println("El nuemero " + numero + " es par");
+        }
+        else{
+            System.out.println("El nuemero " + numero + " es impar");
+        }
+    }
+}
